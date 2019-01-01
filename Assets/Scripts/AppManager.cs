ccba20c5c346694a1416424cec82acd37b92c549
Unknown <yhw7979@naver.com>
2019-01-01 22:50:08 +0900

    1/1 추가사항
    1. 5x5 모양으로 구성(나중에 6x6으로 수정하자)
    2. 버튼 클릭시 비어있는 곳에 랜덤하게 블럭 생성 구현
    
    수정필요
    만들어진 블럭들을 이동하다보면 EmptyArea가 꺼져버리는 문제. 이후 이동도 안되고 드래그 효과도 안됨

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    public List<GameObject> emptyArea; //현재 비어있는 공간
+    public List<GameObject> usedArea; //현재 비어있는 공간
+    public List<GameObject> blocks; //유닛들
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        usedArea = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void MakeBlock()
+    {
+        int random = Random.Range(0, emptyArea.Count);
+
+        GameObject usingBlock = blocks.Find(x => x.gameObject.activeSelf == false); //차후에 블럭 종류에 대해서도 조건을 달아야한다. 현재는 1가지만 있으므로 그냥 꺼져있는 거 가져옴
+        usingBlock.SetActive(true);
+        usingBlock.transform.position = emptyArea[random].transform.position;
+
+        usedArea.Add(emptyArea[random]);
+        emptyArea.RemoveAt(random);
+    }
+}
