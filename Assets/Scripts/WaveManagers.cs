c608749be74006721eb6d382e15b6170299af600
Unknown <yhw7979@naver.com>
2019-01-11 00:30:48 +0900

    1/10 추가사항-2
    1. XML 형식의 텍스트 파일로부터 웨이브 정보를 받아오는 코드 작성(테스트 필요)
    
    테스트필요 : 웨이브 읽어오기, 유닛 불러오기
    
    다음 작업사항
    
    1. 상태이상의 경우 이미지의 변화여부
    2. 적 유닛->appManager->웨이브 매니저 연결관계
    3. 블럭유닛 스크립트 생성

--- Assets/Scripts/WaveManagers.cs
+++ Assets/Scripts/WaveManagers.cs
@@ -1,6 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+public class XmlWaveInfo
+{//xml파일을 읽어서 defunit에 넣기 전에 중간과정(유니티의 xml 파서가 기본형밖에 지원 안 해주기 때문)
+    //스테이지별 모든 유닛의 정보가 담겨있다.
+    public int Wavenow; //현재 웨이브
+    public string waveMapName; //웨이브가 진행 될 맵이름(맵이름이 다를 경우 gameManager에 신 이동 요구)
+
+    public string[] unitName;
+    public int[] numOfUnit;
+}
 
 class WaveUnitInfo //각 웨이브에 등장하는 각 유닛들의 이름과 갯수 등을 저장하는 클래스
 {
@@ -81,7 +92,36 @@ public class WaveManagers : MonoBehaviour
 
     private void LoadWaveData()
     {
+        string[] WaveDataTxt = File.ReadAllText("Assets/GameData/Wave.txt").Split('\n');
 
+        int fileLen = WaveDataTxt.Length;
+
+        if (fileLen < 1)
+        {  //저장된 정보가 있어야 불러옴
+            return;
+        }
+        for (int i = 0; i < fileLen; i++)
+        {
+            if (WaveDataTxt[i] == "")
+            { //빈칸이었을 경우 제외(2중엔터시 나올 수 있음)
+                continue;
+            }
+            XmlWaveInfo tempStageInfo = JsonUtility.FromJson<XmlWaveInfo>(WaveDataTxt[i]);
+
+            List<string> waveEnemyList = new List<string>();
+
+            int enemyLen = tempStageInfo.unitName.Length;
+            for(int j=0; j<enemyLen; j++)
+            {
+                string enemyInfoCombined = tempStageInfo.unitName[j] +'-'+ tempStageInfo.numOfUnit[j]; //enemy의 구성 적군이름(Enemy_OOO)-적유닛 갯수
+                waveEnemyList.Add(enemyInfoCombined);
+            }
+
+            Wave newWave = new Wave();
+            newWave.SetWave(tempStageInfo.Wavenow, waveEnemyList);
+
+            waveInfo.Add(newWave); //새로운 wave정보를 추가
+        }
     }
 
     public void LoadGameData()
